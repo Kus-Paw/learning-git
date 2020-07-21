aefd44a5a819af8da8503ddcdeba69eae58918f6
Kus-Paw <68556631+Kus-Paw@users.noreply.github.com>
2020-07-21 19:24:27 +0200

    Druga zmiana

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 print("hello")
 print("Wpisuje jakąs zmianę")
+
+print ("lista zakupów")
+shopping_dict = {
+"piekarnia" : [ 'chleb'.capitalize(), 'pączek'.capitalize(), 'bułki'.capitalize()],
+"warzywniak" : ['marchew'.capitalize(), 'seler'.capitalize(), 'rukola'.capitalize()]
+}
+#print(shopping_dict.values())
+#print(shopping_dict.keys())
+for product in shopping_dict.keys():  
+    print("Idę do",["piekarnia".capitalize()], "kupuję tu następujące rzeczy:", shopping_dict ["piekarnia"])
+    print("Idę do", ["warzywniak".capitalize()], "kupuję tu następujące rzeczy",shopping_dict ["warzywniak"])
+    break
+print("W sumie kupuję", len(shopping_dict["piekarnia"]) + len(shopping_dict["warzywniak"]), "produktów.")
